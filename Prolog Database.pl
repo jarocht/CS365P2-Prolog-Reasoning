71da36aa562922fb300e33a976ca39b99889d599
Tim Jaroch <tim@8bitelement.com>
2014-03-09 23:24:20 -0400

    Finished Goals and Added Rules. Needs commenting.

--- Prolog Database.pl
+++ Prolog Database.pl
@@ -30,6 +30,12 @@ course(661,'Medical and Bioinformatics','6:00pm-8:50pm','T','EC 612','Dr. J. Lei
 course(671,'Information Visualization','6:00pm-8:50pm','R','EC 612','Dr. J. Leidig','CIS').
 course(691,'MBI Capstone','6:00pm-8:50pm','M','EC 612','Dr. J. Leidig','CIS').
 
+leidig('6:00pm-8:50pm', 'T').
+leidig('6:00pm-8:50pm', 'R').
+leidig('6:00pm-8:50pm','M').
+said('6:00pm-8:50pm','R').
+said('3:00pm-3:50pm','TR').
+
 studentEnrolled('Jim',467).
 studentEnrolled('Jim',452).
 studentEnrolled('Jim',457).
@@ -45,47 +51,90 @@ studentEnrolled('Gaius Baltar',375).
 
 
 /* Rules */
-
 /* Rule: courseName
    Inputs:(courseNum, courseName)
    Description: Pairs course numbers with their names
+	 
 */
 courseName(Num,Name) :- course(Num,Name,_,_,_,_,_).
 
 /* Rule: teaches
-   Inputs:(Professor, courseName)
+   Inputs:(Professor, Name)
    Description: Relates professors and the courses they teach
 
 */
 teaches(Prof,Name):- course(_,Name,_,_,_,Prof,_).
 
+/* Rule: courseTime
+   Inputs:(Name, Time, Day, Prof)
+   Description: Relates courses by course name, time, day of week, and Prof. 
+		Time needs to be an Atom list to allow partial matches within the "Time" Field.
 
+*/
+courseTime(Name, Time, Day, Prof):- 
+		course(_,Name,X,Day,_,Prof,_), atom_codes(X,Y),
+		subset(Time,Y).
 
-/* Goals */
-print_solution :-
-        write('What does Dr. J. Leidig teach?'), nl,
-	findall(X,teaches('Dr. J. Leidig',X),Z),
-	write(Z),nl,nl,
+/* Rule: courseT
+   Inputs:(Name, Time, Day, Prof)
+   Description: Relates courses by course name, time, day of week, and Prof. 
 
-	write('Does Dr. J. Leidig teach Database?'), nl,
-	findall(X,teaches('Dr. J. Leidig','Database'),D),
-	write(D),nl,nl,
+*/		
+courseT(Name, Time, Day, Prof):- course(_,Name,Time,Day,_,Prof,_).
 
-	write('What is Dr. J. Leidigs schedule?'), nl,
-	findall(Crs: Time: Day,course(_,Crs,Day,Time,_,'Dr. J. Leidig',_),Times),
-	write(Times), nl,nl,
+/* Rule: roomTime
+   Inputs:(Room, Time, Day)
+   Description: Relates rooms by room name, time and day of week.
 
-	write('Who is scheduled to teach what subject on TTH, 10am?'), nl,
+*/		
+roomTime(Room,Time,Day,Course):- course(Course,_,Time,Day,Room,_,_).
+		
+/* Rule: courseType
+   Inputs:(Num, Type)
+   Description: Relates course number by course type.
 
-	write('When do Dr. J. Leidig and Dr. El-Said teach at the same time?'), nl,
+*/
+courseType(Num,Type):-course(Num,_,_,_,_,_,Type).
+	
+/*--- Goals ---*/
+/*--- Print Solution ---*/
+:- write('What does Dr. J. Leidig teach?'), nl,
+		findall(X,teaches('Dr. J. Leidig',X),Z),
+		write(Z),nl,nl.
 
-	write('Who teaches at the same time as Dr. J. Leidig?'), nl,
+:- write('Does Dr. J. Leidig teach Database?'), nl,
+		teaches('Dr. J. Leidig','Database') -> write('true'), nl, nl ; write('false'), nl, nl.
 
-	write('What courses fo Jim and Pam have in common?'), nl,
+:- write('What is Dr. J. Leidigs schedule?'), nl,
+		findall(Crs: Time: Day,course(_,Crs,Day,Time,_,'Dr. J. Leidig',_),Times),
+		write(Times),nl,nl.
 
-	write('Who is taking CS courses?'), nl,
+:- write('Who is scheduled to teach what subject on TTH, 10am?'), nl,
+		findall(Name:Prof,courseTime(Name,"10:00am",'TR',Prof),L),
+		write(L),nl,nl.
 
-	write('What types of courses is Gaius Baltar taking?'), nl,
+:- write('When do Dr. J. Leidig and Dr. El-Said teach at the same time?'), nl,
+		leidig(X,A), said(Y,B), X=Y, A=B, write(Y),write(' on '), write(A),nl,nl.
+		
+:- write('Who teaches at the same time as Dr. J. Leidig?'), nl,
+		findall(N,(course(_,_,X,Y,Z,'Dr. J. Leidig',_),course(_,_,A,B,C,N,_),X=A,Y=B,Z=C,N\='Dr. J. Leidig'),L), 
+		write(L),nl,nl.
 
-	write('Are there any scheduling conficts of professors or locations?'), nl.
-
+:- write('What courses do Jim and Pam have in common?'), nl,
+		findall(N, (studentEnrolled('Jim',N), studentEnrolled('Pam', Y), N=Y),L),
+		write(L),nl,nl.
+		
+:- write('Who is taking CS courses?'), nl,
+		setof(N, Y^(studentEnrolled(N,Y),courseType(Y,'CS')),L),
+		write(L),nl,nl.
+		
+:- write('What types of courses is Gaius Baltar taking?'), nl,
+		setof(T, (studentEnrolled('Gaius Baltar',X),courseType(X,T)),L),
+		write(L),nl,nl.
+		
+:- write('Are there any scheduling conficts of professors or locations?'), nl,
+		setof(Prof:Time,(courseT(Name1,Time,Day,Prof),courseT(Name2,Time,Day,Prof), Name1\=Name2),L),
+		write(L),nl,
+		setof(Room:Time,(roomTime(Room,Time,Day,X),roomTime(Room,Time,Day,Y),X\=Y),R),
+		write(R),nl,nl.
+		
